d96c75cda39a817e10f5e3dbe70f46d1bd08e8c3
Rui Paulo Calei <ruipaulocalei0@gmail.com>
2021-09-18 00:23:44 +0100

    Showing all projects

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,12 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    String projectsQuery = """
+    query {
+      projects {
+        id
+        title
+        description
+        }
+      }
+    """;
     return Scaffold(
-        appBar: AppBar(title: const Text('Home Page')),
-        body: Center(child: Text('Home Page')));
+        appBar: AppBar(title: const Text('Projects')),
+        body: Query(
+            options: QueryOptions(document: gql(projectsQuery)),
+            builder: (QueryResult result,
+                {VoidCallback? refetch, FetchMore? fetchMore}) {
+              if (result.hasException) {
+                return Text(result.exception.toString());
+              }
+
+              if (result.isLoading) {
+                return Center(child: CircularProgressIndicator());
+              }
+
+              // it can be either Map or List
+              List projects = result.data!['projects'];
+
+              return ListView.builder(
+                  itemCount: projects.length,
+                  itemBuilder: (context, index) {
+                    final project = projects[index];
+
+                    return Card(
+                      elevation: 8,
+                      child: ListTile(
+                        title: Text(
+                          project['title'],
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        subtitle: Text(
+                          project['description'],
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        trailing: Text(project['id'],
+                            overflow: TextOverflow.ellipsis),
+                      ),
+                    );
+                  });
+            }));
   }
 }
